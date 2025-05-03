c0731012e2b702a3abc3d7774750e5eaaeee48ac
NISHANT KUMAR <abc.nishant007@gmail.com>
2025-05-03 11:07:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ fig.update_layout(
     xaxis=dict(title="TOU Cost ($/kWh)", range=[0, 0.15]),
     yaxis=dict(title="Energy Demand Met (%)", range=[65, 105]),
     height=360,
-    width=360, 
+    width=720, 
     legend=dict(font=dict(size=10)),
     # margin=dict(l=20, r=10, t=30, b=30)
 )
